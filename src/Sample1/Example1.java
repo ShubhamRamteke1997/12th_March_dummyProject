75340566756a31e9930e3c3136997d4495c37d12
shubh <shubh@Shubham>
2022-07-17 23:17:26 +0530

    [Shubham]: 1st Changes in project

---
+++ src/Sample1/Example1.java
@@ -0,0 +1,23 @@
+package Sample1;
+
+public class Example1 {
+public static void main(String[] args) {
+	
+	
+	System.out.println("hello");
+	System.out.println("good morning");
+	System.out.println("How are you");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+}
